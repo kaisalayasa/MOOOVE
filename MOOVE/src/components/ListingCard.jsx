492e823d23a556471fd53d6f0a0cc55ed6c091cd
kaisalayasa <kaisalayasa@gmail.com>
2025-09-27 16:20:30 -0400

    feat: made dynamic rendering from api info

--- MOOVE/src/components/ListingCard.jsx
+++ MOOVE/src/components/ListingCard.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import styles from './ListingCard.module.css'
 import pic from '../assets/listingpic.jpg'
-function ListingCard({size="large",owner=false
+import { IoIosBed } from "react-icons/io";
+import { MdBathroom } from "react-icons/md";
+import { MdDateRange } from "react-icons/md";
+function ListingCard({size="large",owner=false,data
 }) {
 
 
@@ -10,25 +13,30 @@ function ListingCard({size="large",owner=false
   return (
     <div className={`${styles.container} ${sizeClass}`}>
       <div className={styles.tag}>Avallable Now</div>
-      <img src={pic} alt="" />
+      <img src={data?.images?.[0] ?? pic} alt={data?.title ?? 'Listing Image'} />
       <div className={styles.info_container}>
         <div className={styles.main_info}>
           <div>
-            <h2>Cozy Studio Near Campus</h2>
+            <h2>{data?.title ??  'Cozy Studio Near Campus'}</h2>
           </div>
           <div>
-            <h2>$750/mo</h2>
+            <h2>{data?.rent ? ` $${data.rent}/mo` :'$750/mo'}</h2>
           </div>
           
            
 
         </div>
         <div className={styles.meta_data_container}>
-          <p> 0.3 miles from UC Berkely</p>
+          <p> {data?.address?.distanceToCampus ? `${data.address.distanceToCampus} miles` : '0.3 miles'} from {data?.college ??  'UC berkley'}</p>
           <div className={styles.features}>
-            <div> icon bed</div>
-            <div> icon bath</div>
-            <div> icon may-aug</div>
+            <div><IoIosBed/> {data?.bedrooms ?? 1} </div>
+            <div> <MdBathroom /> {data?.bedrooms ?? 1}</div>
+            <div>
+              <MdDateRange />{" "}
+              {data?.availableFrom && data?.availableUntil
+                ? `${new Date(data.availableFrom).toLocaleString('default', { month: 'short', day: 'numeric' })} - ${new Date(data.availableUntil).toLocaleString('default', { month: 'short', day: 'numeric' })}`
+                : 'May 1 - Aug 31'}
+            </div>
 
           </div>
 
