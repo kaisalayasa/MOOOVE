3bee7e46636e71b0e0825d809241dbf2c1ff4701
kaisalayasa <kaisalayasa@gmail.com>
2025-11-05 18:58:49 -0500

    feat: started switching to mongodb

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-require('dotenv').config({path:'../.env'});
 const router = express.Router();
 
 console.log('JWT_SECRET:', process.env.JWT_SECRET);
 console.log('JWT_EXPIRES_IN:', process.env.JWT_EXPIRES_IN);
 
-const users=[];
+const User = require('../models/User');
 const isEduEmail = (email) =>{ return email.toLowerCase().endsWith('.edu');}
 
 router.post('/signup',async (req,res)  =>{
@@ -21,21 +20,28 @@ router.post('/signup',async (req,res)  =>{
     if(!isEduEmail(email)){
         return res.status(400).json({error:'Email has to be .edu'});
     }
-    const existingUser = users.find(u => u.email === email.toLowerCase());
+    const existingUser = User.findOne({email:email.toLowerCase()});
     if(existingUser){
         return res.status(409).json({error:'User Already Exists Sign in Please.'});
     }
     const passwordHash = await bcrypt.hash(password,10);
-    const user = {id:users.length+1, email: email.toLowerCase(), password: passwordHash};
+    const user = await User.create({
+        email: email.toLowerCase(),
+        password: passwordHash
+    })
  
     const token = jwt.sign(
-        {sub:user.id , email:user.email},
+        {sub:user._id.toString() , email:user.email},
         process.env.JWT_SECRET,
         {expiresIn: process.env.JWT_EXPIRES_IN}
         
     )
-    users.push(user)
+   
     res.status(201).json({message:"User created",token})
+    if (err.code === 11000) {
+         return res.status(409).json({ error: 'User already exists' });
+        }
+
 
 
 })
@@ -45,7 +51,7 @@ router.post('/login',async (req,res)=>{
     if(!email || !password){
         return res.status(400).json({error:"Email and password requried."});
     }
-    const user = users.find((u)=> u.email === email.toLowerCase());
+    const user = await User.findOne({email: email.toLowerCase()});
     if(!user){
         return res.status(400).json({error:"invalid credentials"})
     }
